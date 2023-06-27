cdd6e1826c5e8c0c56dd91400bd2d4d919fea2bb
jaimejuniordt <jaimejuniordt@gmail.com>
2023-06-26 23:31:38 -0300

    circleStyle

--- homepagereact/src/components/Elipse/index.js
+++ homepagereact/src/components/Elipse/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import style from './style.css';
 
 export default function Elipse({size, opacity, style}) {
+  const circleStyle = {
+    width: '${size} px',
+    height:'${size} px',
+    opacity: opacity ,
+  }
+
+
+
   return(
   <>
   <div className={'circle'}></div>
