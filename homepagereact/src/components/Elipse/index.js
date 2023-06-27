dfc89c931af0bd61c0c305a2b6762fa5fdf5348d
jaimejuniordt <jaimejuniordt@gmail.com>
2023-06-26 23:19:12 -0300

    circles.working

--- homepagereact/src/components/Elipse/index.js
+++ homepagereact/src/components/Elipse/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './style.css';
 
-export default function Elipse() {
+export default function Elipse({size, opacity, style}) {
   return(
   <>
   <div className={'circle'}></div>
