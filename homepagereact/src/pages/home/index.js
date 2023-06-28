e069cc6e6fe27408ad5ca35bbeb2e4d590a8a7d9
jaimejuniordt <jaimejuniordt@gmail.com>
2023-06-27 23:54:33 -0300

    style.css

--- homepagereact/src/pages/home/index.js
+++ homepagereact/src/pages/home/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
 //MY IMPORTS
-import style from './style.css';
-import Elipse from '../../components/Elipse';
+import style from "./style.css";
+import Elipse from "../../components/Elipse";
 
 function Home() {
-  return(
+  return (
     <>
-   <header className='container'>
-    {/* Elipse -> Circle */}
-    <div className='elipse-container'>
-      <div className='elipse-left'></div>
-      <Elipse size={120} opacity={0.2} style={'cicle-left-one'} />
-      <Elipse size={180} opacity={0.4} style={'cicle-left-two'} />
+      <header className="container">
+        {/* Elipse -> Circle */}
+        <div className="elipse-container">
+          <div className="elipse-left">
+            <Elipse size={120} opacity={1} style={"circle-left-one"} />
+            <Elipse size={180} opacity={0.4} style={"circle-left-two"} />
+          </div>
 
-      <div className='elipse-right'></div>
-    </div>
-    
-    </header>   
+          <div className="elipse-right"></div>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 export default Home;
