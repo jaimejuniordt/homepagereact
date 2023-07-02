020b3953701bcd110f8a56bed5887fbc62e6642e
jaimejuniordt <jaimejuniordt@gmail.com>
2023-07-01 23:54:30 -0300

    information

--- homepagereact/src/pages/home/index.js
+++ homepagereact/src/pages/home/index.js
@@ -18,50 +18,36 @@ function Home() {
           <div className="elipse-right">
             <Elipse size={120} opacity={0.2} styles={"circle-right-one"} />
             <Elipse size={120} opacity={0.5} styles={"circle-right-two"} />
-            <Elipse size={180} opacity={0.6} styles={"circle-right-three"}/>
+            <Elipse size={180} opacity={0.6} styles={"circle-right-three"} />
             <Elipse size={180} opacity={0.98} styles={"circle-right-four"} />
-            </div>
+          </div>
 
-            <div className='information-container'>
-              <div className='information-left'>
-                <div className='information-text'>
-                  <h3>Descubra</h3>
-                  <h1>
-                    Transforme sua alimentação e <br/>
-                    tenha mais qualidade de vida!
-                  </h1>
+          <div className="information-container">
+            <div className="information-left">
+              <div className="information-text">
+                <h3>Descubra</h3>
+                <h1>
+                  Transforme sua alimentação e <br />
+                  tenha mais qualidade de vida!
+                </h1>
 
-                  <p>
-                    egegveghthetththhtthhth
-                  </p>
+                <p>egegveghthetththhtthhth</p>
 
-                  <h4>
-                    Não perca a oportunidade de mudar seus hábitos
-                  </h4>
+                <h4>Não perca a oportunidade de mudar seus hábitos</h4>
 
-                  <p>
-                    jojvjiojoiuiosjsvjoisjoif
-                  </p>
+                <p>jojvjiojoiuiosjsvjoisjoif</p>
 
-                  <p>
-                    jojvjiojoiuiosjsvjoisjoif
-                  </p>
+                <p>jojvjiojoiuiosjsvjoisjoif</p>
 
-                  <p>
-                    jojvjiojoiuiosjsvjoisjoif
-                  </p>
-
-                </div>
+                <p>jojvjiojoiuiosjsvjoisjoif</p>
               </div>
-
-              <div className='information-right'></div>
-
             </div>
 
-          
+            <div className="information-right"></div>
+          </div>
         </div>
       </header>
     </>
-  )
+  );
 }
 export default Home;
