e1b8f8a12f5850cf73226a0509db141e100997a3
jaimejuniordt <jaimejuniordt@gmail.com>
2023-07-07 21:03:42 -0300

    contacts

--- homepagereact/src/pages/home/index.js
+++ homepagereact/src/pages/home/index.js
@@ -1,21 +1,26 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 //MY IMPORTS
 import styles from "./styles.css";
-import mobile from '../mobile.css';
+import mobile from "../mobile.css";
 import Elipse from "../../components/Elipse";
 import book from "../../image/book.jpg";
 
-
-
 function Home() {
-  const [size, setSize]= useState(window.innerWidth);  
+  const [size, setSize] = useState(window.innerWidth);
+
+  const instagram = "@professorjotaj";
+
+  const whatsapp = "64-992968868";
+
+  const site = "site";
+
   useEffect(() => {
     windowSize();
   }, []);
 
-  function windowSize(){
-    setSize(window.innerWidth);    
+  function windowSize() {
+    setSize(window.innerWidth);
   }
 
   return (
@@ -62,62 +67,89 @@ function Home() {
 
           <div className="information-right">
             <form>
-
               <div className="form-input">
-                <input type="text" className='input' id='firstName' />
-                <label htmlFor='firstName' className='label-input'> Primeiro nome*</label>
+                <input type="text" className="input" id="firstName" />
+                <label htmlFor="firstName" className="label-input">
+                  {" "}
+                  Primeiro nome*
+                </label>
               </div>
 
               <div className="form-input">
-                <input type="text" className='input' id='lastName' />
-                <label htmlFor='lastName' className='label-input'> Sobrenome*</label>
+                <input type="text" className="input" id="lastName" />
+                <label htmlFor="lastName" className="label-input">
+                  {" "}
+                  Sobrenome*
+                </label>
               </div>
 
               <div className="form-input">
-                <input type="text" className='input' id='email' />
-                <label htmlFor='email' className='label-input'> E-mail*</label>
-              </div> 
+                <input type="text" className="input" id="email" />
+                <label htmlFor="email" className="label-input">
+                  {" "}
+                  E-mail*
+                </label>
+              </div>
 
-              <div className='form-text'>
+              <div className="form-text">
                 <span>
-                  Inscreva-se para receber em primeira mão as melhores aulas que farão você realmente aprender Português.
+                  Inscreva-se para receber em primeira mão as melhores aulas que
+                  farão você realmente aprender Português.
                 </span>
               </div>
 
-              <div className='form-enquete'>
-                <input type="radio" id='formYes' name='option' />
-                <label htmalFor='formYes'>Sim, eu quero me inscrever!</label>                
+              <div className="form-enquete">
+                <input type="radio" id="formYes" name="option" />
+                <label htmalFor="formYes">Sim, eu quero me inscrever!</label>
               </div>
 
-              <div className='form-enquete'>
-                <input type="radio" id='formNo' name='option' />
-                <label htmalFor='formNo'>Não, estou bem obrigado!</label>                
+              <div className="form-enquete">
+                <input type="radio" id="formNo" name="option" />
+                <label htmalFor="formNo">Não, estou bem obrigado!</label>
               </div>
 
-              <button className='form-button'>Download Aulas</button>
-
-
-
+              <button className="form-button">Download Aulas</button>
             </form>
           </div>
         </div>
       </header>
 
       <main>
-        <section className='services'>
+        <section className="services">
           <span>
-            O método de ensino do prof. Jota J é o mais prático possível. Ele , através de pesquisas, desenvolveu um jeito de ensinar que elimina a maior parte das ditas regras. Como sua aula de "Crase". O estudante aprende uma maneira que não precisa decorar os mais de 10 casos de crase obrigatória, sem falar dos casos de crase facultativa. Seu método prático, cativa o aluno e mostra que Português não é um "bicho de 7 cabeças", tornando assim, nossa Língua Portuguesa agradável de ser estudada.
+            O método de ensino do prof. Jota J é o mais prático possível. Ele ,
+            através de pesquisas, desenvolveu um jeito de ensinar que elimina a
+            maior parte das ditas regras. Como sua aula de "Crase". O estudante
+            aprende uma maneira que não precisa decorar os mais de 10 casos de
+            crase obrigatória, sem falar dos casos de crase facultativa. Seu
+            método prático, cativa o aluno e mostra que Português não é um
+            "bicho de 7 cabeças", tornando assim, nossa Língua Portuguesa
+            agradável de ser estudada.
           </span>
         </section>
 
-        <section className='social-media'>
-          <div className='contact'>
-          <a href='' target='blank'>
+        <section className="social-media">
+          <div className="contact">
+            <a href="instagram" target="blank">
+              <ion-icon name="logo-instagram"></ion-icon>
+              <span>@Professor JotaJ</span>
+            </a>
+          </div>
 
+          <div className="contact">
+            <a href="whatsapp" target="blank">
+              <ion-icon name="logo-whatsapp"></ion-icon>
+              <span>64-99296-8868</span>
+            </a>
+          </div>
+
+          <div className="contact">
+            <a href="site" target="blank">
+              <ion-icon name="pencil-outline"></ion-icon>
+              <span>www.site</span>
             </a>
           </div>
         </section>
-
       </main>
     </>
   );
